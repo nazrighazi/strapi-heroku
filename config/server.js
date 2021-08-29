c3e41d22842e0c6ca0767a30a7cd11954ad62e1f
Nazri Ghazi <nazri.ghazi@celcom.com.my>
2021-08-30 01:01:35 +0800

    add server

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL"),
+  host: env("host", "0.0.0.0"),
+  port: env.int("port", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "839ee8b83a838156101f00bd425852c9"),
+    },
+  },
 });
