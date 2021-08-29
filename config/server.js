7df5ef25f206840573e283a3fed9da4b0e603006
Nazri Ghazi <nazri.ghazi@celcom.com.my>
2021-08-30 00:19:19 +0800

    git commit -m "Update database config"

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '839ee8b83a838156101f00bd425852c9'),
-    },
-  },
+  url: env("MY_HEROKU_URL"),
 });
